cf557926e0b0d2b92bfa60fb1a8edc86c6dccb6f
CoreMamba <215352410+CoreMamba@users.noreply.github.com>
2025-07-09 13:09:09 +0000

    second.sql

--- second.sql
+++ second.sql
@@ -1,15 +1,10 @@
--- show databases;
-
--- USE Parks_and_Recreation;
--- SHOW TABLES FROM Parks_and_Recreation;
-
--- USE Parks_and_Recreation;
--- SHOW TABLES;
--- USE Parks_and_Recreation;
-
-SELECT * 
-FROM employee_demographics
-WHERE employee_id = 1 AND employee_id = 2;
+-- show databases;-- USE Parks_and_Recreation;-- SHOW TABLES FROM Parks_and_Recreation;-- USE Parks_and_Recreation;-- SHOW TABLES;-- USE Parks_and_Recreation;
+-- UNIONS
+SELECT first_name, last_name
+ FROM employee_demographics
+ UNION ALL
+SELECT first_name, last_name
+ FROM employee_salary;
 
 
 
