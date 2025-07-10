e71967ddfe5ddd75d2cd3a1a3011dd4ef7e2395d
CoreMamba <215352410+CoreMamba@users.noreply.github.com>
2025-07-10 14:26:53 +0000

    Add second.sql

--- second.sql
+++ second.sql
@@ -1,10 +1,13 @@
 -- show databases;-- USE Parks_and_Recreation;-- SHOW TABLES FROM Parks_and_Recreation;-- USE Parks_and_Recreation;-- SHOW TABLES;-- USE Parks_and_Recreation;
 -- UNIONS
-SELECT first_name, last_name
+/*
  FROM employee_demographics
  UNION ALL
 SELECT first_name, last_name
- FROM employee_salary;
-
-
-
+ FROM employee_salary; */
+-- SHOW TABLES FROM Parks_and_Recreation;
+-- SELECT * FROM employee_demographics;
+-- SELECT * FROM employee_salar; 
+-- USE Parks_and_Recreation;
+SELECT * FROM parks_departments;
+-- SHOw TABLES from Parks_and_Recreation
